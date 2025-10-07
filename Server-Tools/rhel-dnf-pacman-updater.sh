8e268ce8ac3087fa2f3c4a53e621827ee0237068
andrewarabian <andrew.arabian.104@my.csun.edu>
2025-10-07 13:43:29 -0700

    10.7.25 RHEL repo updater

--- Server-Tools/rhel-dnf-pacman-updater.sh
+++ Server-Tools/rhel-dnf-pacman-updater.sh
@@ -1,4 +1,6 @@
-#Auto Updater
+#Simple Auto Updater for RHEL environments
 #!/bin/bash
+echo "Updating dnf and flatpak repositories.."
 sudo dnf update && dnf upgrade
 sudo flatpak update && flatpak upgrade
+echo "All repositories updated."
