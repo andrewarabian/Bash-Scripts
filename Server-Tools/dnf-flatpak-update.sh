8dfa206fb5a5157a156d905f0c6916f00244b894
andrewarabian <andrew.arabian.104@my.csun.edu>
2025-10-07 13:52:20 -0700

    10.7.25 Simple AIO script to update dnf and flatpak repositories.

--- Server-Tools/dnf-flatpak-update.sh
+++ Server-Tools/dnf-flatpak-update.sh
@@ -1,4 +1,4 @@
-#Simple Auto Updater for RHEL environments.
+#Simple update script for dnf and flatpak repositories (Standard in most RHEL environments)
 #!/bin/bash
 echo "Updating dnf and flatpak repositories.."
 sudo dnf update && dnf upgrade
